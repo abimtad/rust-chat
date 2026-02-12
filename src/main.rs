31174cd9e386d33d988e0d6797a663d4c3b76f7c
Abel Tadesse <tadesseabel26@gmail.com>
2026-02-12 12:46:44 +0300

    Extract handshake response builder

--- src/main.rs
+++ src/main.rs
@@ -5,37 +5,16 @@ extern crate rustc_serialize;
 
 use std::cell::RefCell;
 use std::collections::HashMap;
-use std::fmt;
 use std::net::SocketAddr;
 use std::rc::Rc;
 
 use mio::*;
 use mio::tcp::*;
 use http_muncher::{Parser, ParserHandler};
-use rustc_serialize::base64::{ToBase64, STANDARD};
+mod handshake;
 
-fn gen_key(key: &str) -> String {
-    let mut m = sha1::Sha1::new();
-    let mut buf = [0u8; 20];
+use handshake::build_handshake_response;
 
-    m.update(key.as_bytes());
-    m.update("258EAFA5-E914-47DA-95CA-C5AB0DC85B11".as_bytes());
-
-    m.output(&mut buf);
-
-    buf.to_base64(STANDARD)
-}
-
-fn build_handshake_response(headers: &HashMap<String, String>) -> String {
-    let response_key = gen_key(headers.get("Sec-WebSocket-Key").unwrap());
-    fmt::format(format_args!(
-        "HTTP/1.1 101 Switching Protocols\r\n\
-         Connection: Upgrade\r\n\
-         Sec-WebSocket-Accept: {}\r\n\
-         Upgrade: websocket\r\n\r\n",
-        response_key
-    ))
-}
 
 struct HttpParser {
     current_key: Option<String>,
