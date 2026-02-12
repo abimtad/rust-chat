f6d6d3dd62163f7fadf697b1449036a79f374556
Abel Tadesse <tadesseabel26@gmail.com>
2026-02-12 13:11:39 +0300

    Extract websocket client

--- src/main.rs
+++ src/main.rs
@@ -3,89 +3,18 @@ extern crate http_muncher;
 extern crate sha1;
 extern crate rustc_serialize;
 
-use std::cell::RefCell;
-use std::collections::HashMap;
 use std::net::SocketAddr;
-use std::rc::Rc;
 
 use mio::*;
 use mio::tcp::*;
-use http_muncher::Parser;
 mod handshake;
 mod http_parser;
+mod client;
 
-use handshake::build_handshake_response;
-use http_parser::HttpParser;
+use client::WebSocketClient;
 
 
 
-#[derive(PartialEq)]
-enum ClientState {
-    AwaitingHandshake,
-    HandshakeResponse,
-    Connected,
-}
-
-struct WebSocketClient {
-    socket: TcpStream,
-    headers: Rc<RefCell<HashMap<String, String>>>,
-    http_parser: Parser<HttpParser>,
-    interest: EventSet,
-    state: ClientState,
-}
-
-impl WebSocketClient {
-    fn new(socket: TcpStream) -> WebSocketClient {
-        let headers = Rc::new(RefCell::new(HashMap::new()));
-
-        WebSocketClient {
-            socket: socket,
-            headers: headers.clone(),
-            http_parser: Parser::request(HttpParser::new(headers.clone())),
-            interest: EventSet::readable(),
-            state: ClientState::AwaitingHandshake,
-        }
-    }
-
-    fn write(&mut self) {
-        let headers = self.headers.borrow();
-        let response = build_handshake_response(&headers);
-        self.socket.try_write(response.as_bytes()).unwrap();
-
-        // Change the state
-        self.state = ClientState::Connected;
-
-        self.interest.remove(EventSet::writable());
-        self.interest.insert(EventSet::readable());
-    }
-
-    fn read(&mut self) {
-        loop {
-            let mut buf = [0; 2048];
-            match self.socket.try_read(&mut buf) {
-                Err(e) => {
-                    println!("Error while reading socket: {:?}", e);
-                    return;
-                }
-                Ok(None) =>
-                    // Socket buffer has got no more bytes.
-                    break,
-                Ok(Some(_len)) => {
-                    self.http_parser.parse(&buf);
-                    if self.http_parser.is_upgrade() {
-                        // Change the current state
-                        self.state = ClientState::HandshakeResponse;
-
-                        // Change current interest to `Writable`
-                        self.interest.remove(EventSet::readable());
-                        self.interest.insert(EventSet::writable());
-                        break;
-                    }
-                }
-            }
-        }
-    }
-}
 
 struct WebSocketServer {
     socket: TcpListener,
